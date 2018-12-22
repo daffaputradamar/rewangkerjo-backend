6e86dadd29e4848ffc28dd7ae8fbe1a29c3456c7
agusprasetyo1 <agusprasetyo1889@gmail.com>
2018-12-22 13:31:09 +0700

    Peserta

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { SampleRouter } from './routers/SampleRouter'
 import { SupportedByRouter } from './routers/SupportedByRouter'
 import { KontakRouter } from './routers/KontakRouter'
+import { PesertaRouter } from './routers/PesertaRouter'
 
 const app = express()
 
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use('/sample', SampleRouter)
 app.use('/supportedBy', SupportedByRouter)
 app.use('/kontak', KontakRouter)
+app.use('/peserta', PesertaRouter)
 
 app.listen(3000, () => console.log('service listening'))
