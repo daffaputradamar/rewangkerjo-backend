fc68c85aa7baaf197104c063897fe84bd93b2b58
daffaputradamar <daffaputradamar@gmail.com>
2018-12-22 13:14:28 +0700

    add User service

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import { SampleRouter } from './routers/SampleRouter'
-import { SupportedByRouter } from './routers/SupportedByRouter'
-import { KontakRouter } from './routers/KontakRouter'
+import express from "express"
+import cors from "cors"
+import { SampleRouter } from "./routers/SampleRouter"
+import { SupportedByRouter } from "./routers/SupportedByRouter"
+import { KontakRouter } from "./routers/KontakRouter"
+import { UserRouter } from "./routers/UserRouter"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use('/sample', SampleRouter)
-app.use('/supportedBy', SupportedByRouter)
-app.use('/kontak', KontakRouter)
+app.use("/sample", SampleRouter)
+app.use("/users", UserRouter)
+app.use("/supportedBy", SupportedByRouter)
+app.use("/kontak", KontakRouter)
 
-app.listen(3000, () => console.log('service listening'))
+app.listen(3000, () => console.log("service listening"))
---
+++ models/User.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose"
+
+const UserSchema = new Schema({
+  username: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  level: {
+    type: String
+  }
+})
+
+export const User = model("User", UserSchema)
