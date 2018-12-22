62bf7bcf4563d805e4e8e691daa0850da5c1f3e8
najibulhoir <najibulhoir98@gmail.com>
2018-12-22 15:18:01 +0700

    Universitas

--- routers/UniversitasRouter.ts
+++ routers/UniversitasRouter.ts
@@ -4,6 +4,7 @@ import { UniversitasController } from '../controllers/UniversitasController'
 const router = Router()
 const controller = new UniversitasController()
 
+
 router.get('/:id', controller.show)
 
 export const UniversitasRouter = router
