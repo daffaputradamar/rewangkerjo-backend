5ed16e6308847cc0b47155540810c5f8f6541f59
najibulhoir <najibulhoir98@gmail.com>
2018-12-22 15:17:43 +0700

    Universitas

--- controllers/UniversitasController.ts
+++ controllers/UniversitasController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { Universitas } from "../models/Universitas"
 
+
 export class UniversitasController {
     public show(req: Request, res: Response){
         Universitas.findById(req.params.id).then((data) => res.json(data));
