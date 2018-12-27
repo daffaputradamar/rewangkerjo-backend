e74016ebb1915ddda5c23abd78814da38c5f710a
najibulhoir <najibulhoir98@gmail.com>
2018-12-27 20:24:19 +0700

    seeder universitas

---
+++ seeders/universitas.ts
@@ -0,0 +1,11 @@
+export default[
+    {
+        nama : 'Politeknik Negeri Malang'
+    },
+    {
+        nama : 'Universitas Brawijaya'
+    },
+    {
+        nama : 'Universitas Negeri Malang'
+    }
+]
