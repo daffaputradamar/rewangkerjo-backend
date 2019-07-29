e35853aaab9ebd687be3e159b06fd6b690b26b82
Naufal Yudhistira <yudhistn@gmail.com>
2019-07-29 09:05:03 +0700

    Change .env

--- config/db.ts
+++ config/db.ts
@@ -4,11 +4,16 @@ const connectionString = `mongodb+srv://wri:${
   process.env.PASSWORD
 }@cluster0-enygr.mongodb.net/test?retryWrites=true`
 
+console.log(connectionString)
 connect(
   connectionString,
   {
     useNewUrlParser: true,
   },
-).then(() => {
-  console.log("Connected to mongodb")
-})
+)
+  .then(() => {
+    console.log("Connected to mongodb")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
